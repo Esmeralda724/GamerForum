b031085e601a6b3efc4f0b8d99a6edeeeb8c848b
Esmeralda724 <emmi.cortez2@gmail.com>
2018-05-15 17:44:20 -0700

    update3

--- src/app/gamerforum/gamerforum.component.ts
+++ src/app/gamerforum/gamerforum.component.ts
@@ -1,15 +1,27 @@
 import { Component, OnInit } from '@angular/core';
 
+class Status {
+  content: String;
+}
 @Component({
   selector: 'app-gamerforum',
   templateUrl: './gamerforum.component.html',
   styleUrls: ['./gamerforum.component.css']
 })
 export class GamerforumComponent implements OnInit {
-
+  public Status = new Status;
+  public post = [];
+  public goalText: String = 'My first life goal';
   constructor() { }
 
   ngOnInit() {
   }
 
+  addItem() {
+  console.log(this.Status);
 }
+
+
+}
+
+
